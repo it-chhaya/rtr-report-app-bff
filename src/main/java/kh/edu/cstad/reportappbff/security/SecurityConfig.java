3f9d6c1eda74680fe79f2a3bcead0b9b45a6fcc4
techmentors2024 <techmentors2024@gmail.com>
2025-08-15 09:41:31 +0700

    final

--- src/main/java/kh/edu/cstad/reportappbff/security/SecurityConfig.java
+++ src/main/java/kh/edu/cstad/reportappbff/security/SecurityConfig.java
@@ -1,15 +1,26 @@
 package kh.edu.cstad.reportappbff.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String issuerUri;
+
+    @Value("${spring.security.oauth2.client.registration.nextjs.client-id}")
+    private String clientId;
 
     @Bean
     public SecurityWebFilterChain configureFilterChain(ServerHttpSecurity http) {
@@ -27,7 +38,26 @@ public class SecurityConfig {
 
         http.oauth2Login(Customizer.withDefaults());
 
+        http.logout(logoutSpec ->
+                logoutSpec.logoutUrl("/logout")
+                        .logoutSuccessHandler(logoutSuccessHandler())
+        );
+
         return http.build();
+    }
+
+    ServerLogoutSuccessHandler logoutSuccessHandler() {
+        RedirectServerLogoutSuccessHandler redirectServerLogoutSuccessHandler = new RedirectServerLogoutSuccessHandler();
+
+        String keycloakLogoutUrl = issuerUri + "/protocol/openid-connect/logout" +
+                "?client_id=" + clientId +
+                "&post_logout_redirect_uri=http://localhost:8000/";
+
+
+        URI logoutSuccessUrl = URI.create(keycloakLogoutUrl);
+        redirectServerLogoutSuccessHandler.setLogoutSuccessUrl(logoutSuccessUrl);
+
+        return redirectServerLogoutSuccessHandler;
     }
 
 }
